aa6e3e355314dde13d1f12cd6556eaf45c12ba04
Kristjan Metsalu <kristjan.metsalu@ut.ee>
2014-08-12 17:19:43 +0300

    added require

--- lib/simple_tables.rb
+++ lib/simple_tables.rb
@@ -3,3 +3,6 @@ require "simple_tables/version"
 module SimpleTables
   # Your code goes here...
 end
+
+require File.dirname(__FILE__) + '/simple_tables/table.rb'
+require File.dirname(__FILE__) + '/simple_tables/pivot_table.rb'
